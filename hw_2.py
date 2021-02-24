12ca901e565d829b28b97edd027035b1ce0d6c36
Pavel Soloshenko <apodisation13@gmail.com>
2021-02-24 18:24:20 +0300

    hw2

---
+++ hw_2.py
@@ -0,0 +1,27 @@
+from cook_book import cook_book
+
+
+def get_shop_list_by_dishes(dishes: list, person_count: int, cook: dict):
+    d = {}
+    for dish in dishes:
+        if dish in cook:
+            for ingredient in cook[dish]:
+                # print(ingredient)
+                if ingredient['ingredient_name'] not in d:
+                    d[ingredient['ingredient_name']] = \
+                        {'measure': ingredient['measure'], 'quantity': ingredient['quantity'] * person_count}
+                else:
+                    quantity = d[ingredient['ingredient_name']]['quantity']
+                    d[ingredient['ingredient_name']] = \
+                        {'measure': ingredient['measure'],
+                         'quantity': (ingredient['quantity'] * person_count) + quantity}
+        else:
+            print(f'Ошибка: нельзя добавить блюдо {dish}. Оно отсутствует в книге')
+    print(d)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
+get_shop_list_by_dishes(["Омлет", "Фахитос"], 3, cook_book)
+# для этой комбинации повторяется помидор, поэтому его вышло 12: 2*3 + 2*3
+get_shop_list_by_dishes(['Утка по-пекински', 'Омлетт'], 2, cook_book)
+# утку посчитает, а во втором элементе ошибка
